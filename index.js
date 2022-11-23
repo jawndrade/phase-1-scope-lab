c1abb480f9bb04020b4c4fd98f508a21212700ff
Jessica <andradejsscl@gmail.com>
2022-11-23 12:10:32 -0500

    commit tree c075a44b91b9e332cba3df278a3622479b295fe7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(name) {
+    bestCustomer = name
+}
+// console.log(bestCustomer)
+
+const leastFavoriteCustomer = 'Benjamin'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Rob'
+}
+// console.log(leastFavoriteCustomer)
